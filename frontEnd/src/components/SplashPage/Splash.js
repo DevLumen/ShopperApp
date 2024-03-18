e5d0ddf336160278a8c5fc66a8eb61ed37c9c16c
Andres S <devdre783@Andress-MacBook-Pro.local>
2024-03-18 13:02:11 -0700

    add form to splash page for login

--- frontEnd/src/components/SplashPage/Splash.js
+++ frontEnd/src/components/SplashPage/Splash.js
@@ -1,7 +1,13 @@
 export default function Splash() {
-    return (
-      <div>
-        <h1>HELLO WORLD</h1>
-      </div>
-    );
+  return (
+    <div>
+      <form action="" method="post">
+        <label for="username">Username:</label>  
+        <input id="username" type="text" name="username" placeholder="Username" required/>
+        <label for="password">Password:</label> 
+        <input id="password" type="password" name="password" placeholder="Password" required/>
+        <button type="submit" value="login">Submit</button>
+      </form>
+    </div>
+  );
 }

dd80565ff40e16ef3124f0f37e97d4787462e0d9
Andres S <devdre783@Andress-MacBook-Pro.local>
2024-03-22 12:47:05 -0700

    set up the backend connection to mongo using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config({path: './config.env'});
 import mongoose from 'mongoose';
-
+import User from './models/userModel.js';
 
 const app = express();
+const PORT = 5000;
+const DB_URI = "mongodb+srv://devdre783:Paramus.1@cluster0.gpza9p8.mongodb.net/shopperDB";
 
-const DB_URI = process.env.DATABASE;
-console.log(DB_URI)
-mongoose.connect(DB_URI, {
+await mongoose.connect(DB_URI, {
 }).then(() => {console.log(`DB connection successful`)})
 
-const PORT = process.env.PORT || 3000;
+app.get("*", async (req, res) => {
+    console.log("HELLO");
+    const user = await User.find({});
+    console.log("HELLO2");
+    res.send(user);
+});
+
 
 app.listen(PORT, () => console.log(`Server is listening ${PORT}`))
